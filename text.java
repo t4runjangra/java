950df7c346f41f44864b8a353ebe430ff7b50a67
tarun <tarunjangra1903@gmail.com>
2025-10-15 18:01:28 +0530

    datatypes is java

--- text.java
+++ text.java
@@ -1,3 +1,18 @@
-static void main(String[] args) {
-    System.out.println("hello ");
-}
+//first java program 
+
+// static void main(String[] args) {
+//     System.out.println("hello ");
+// }
+
+
+//data types in java
+  public static void main(String[] args) {
+    String name = "Tarun"; //String: depends on length; each character is 2 bytes plus object overhead  size depends on the length (5 characters × 2 bytes each = 10 bytes for characters + some object overhead) String is an object, not a primitive type. It stores a sequence of characters internally as a char array.
+    int age = 19; //requires 4 bytes
+    float cgpa = 8.5f ; //requires 4 bytes
+    char letter = 'T'; //requires 2 bytes
+    boolean myBool = true; //requires typically 1 byte
+    double value = 19.99; //requires 8 bytes
+    System.out.println("Hey! i am "+name +"\nI am " + age + "old\n" + "and i got" + cgpa + "\nMy name starts form the letter "+ letter +" \nand all the information about me is "+ myBool+ "\n" + value);
+  }
+
