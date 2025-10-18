8a263860ff725614b6de3eb569e523c85ca7b3a5
tarun <tarunjangra1903@gmail.com>
2025-10-18 23:01:45 +0530

    more classes in java

--- Main.java
+++ Main.java
@@ -75,7 +75,7 @@
 //   protected String email = "john@doe.com";
 //   protected int age = 24;
 //   public static void main(String[] args) {
-    
+
 //   }
 // }
 
@@ -90,18 +90,150 @@
 //   }
 // }
 
-abstract class Main {
-    public String name = "tarun";
-    public int age = 19;
-    protected abstract void study();
+// abstract class Main {
+//     public String name = "tarun";
+//     public int age = 19;
+//     protected abstract void study();
+
+// }
+
+// class person extends Main{
+//     public int year = 2024;
+//     public void study(){
+//         System.out.println("studying all day");
+//     }
+
+// }
+
+// Encapsulation
+
+//  public class Main {
+//       private String name;
+
+//     //  getter
+
+//      public String getName() {
+//          return name;
+//      }
+
+//   //  setter
+
+//     public void setName(String newName) {
+//          this.name = newName;
+//     }
+
+//      public static void main(String[] args) {
+//          Main myObj = new Main();
+//          myObj.setName("tarun");
+//          String name = myObj.getName();
+//          System.out.println(name);
+//      }
+
+//   }
+
+// Packages:- Packages in java is used to group classes, it is like a file in a directory you can import it in your program. we use packages to avoid name conflicts. and to wrtie a better maintainable code 
+// built in packages 
+// user define packeages 
+
+// import java.util.Scanner;
+
+// public class Main {
+//     public static void main(String[] args) {
+//         Scanner scanner = new Scanner(System.in);
+//         System.out.print("enter your Name: ");
+//         String username = scanner.nextLine();
+//         System.out.println(username);
+
+//     }
+
+// }
+
+// inheritence 
+
+// public class Main {
+//     protected String name = "Ford";
+//     public void honk(){
+//         System.out.println("Poo Poo!");
+//     }
+// }
+// class car extends Main {
+//     private String modelName = " Mustang";
+//     public static void main(String[] args) {
+//             car myObj = new car();
+//             System.out.println(myObj.name +" " + myObj.modelName);
+//             myObj.honk();
+//     }
+
+// }    
+
+// polymorphism
+
+// class animal {
+//     public void animalSound(){
+//         System.out.println("Animal sound");
+//     }
+// }
+// class pig extends animal {
+//     public void pigSound(){
+//         System.out.println("Pig sound");
+//     }
+// }
+// class dog extends animal{
+//     public void dogSound(){
+//         System.out.println("Dog sound");
+//     }
+// }
+// class Main {
+//     public static void main(String[] args) {
+//         animal obj1 = new animal();
+//         pig obj2 = new pig();
+//         dog obj3 = new dog();
+//         obj1.animalSound();
+//         obj2.pigSound();
+//         obj3.dogSound();
+//     }
     
+// }
+
+
+
+// super keyword // used to refer the parent class of subclass , most common use case of it to avoid the conflict between superclass and subclass if they contain same name method.
+
+
+
+// public class Main {
+
+//     public void animalSound(){
+//         System.out.println("sound");
+//     }    
+// }
+
+// class sound extends Main{
+//     public void animalSound(){
+//         super.animalSound();
+//         System.out.println("another sound");
+//     }
+// }
+// class run{
+//     public static void main(String[] args) {
+//         sound obj = new sound();
+//         obj.animalSound();
+//     }
+// }
+
+
+public class Main {
+    int x = 5;
+    public class InnerMain {
+        int y = 3;
+    }
 }
 
-class person extends Main{
-    public int year = 2024;
-    public void study(){
-        System.out.println("studying all day");
+class a {
+    public static void main(String[] args) {
+        Main obj = new Main();
+        Main.InnerMain obj1 = obj.new InnerMain();
+        System.out.println(obj1.y +" "+ obj.x);
     }
     
 }
-
--- second.java
+++ second.java
@@ -1,9 +1,9 @@
-public class second {
-        public static void main(String[] args) {
-            person myObj = new person();
-            System.out.println("\n"+ myObj.name);
-            System.out.println(myObj.age);
-            System.out.println(myObj.year);
-            myObj.study();
-        }
-}
+// public class second {
+//         public static void main(String[] args) {
+//             person myObj = new person();
+//             System.out.println("\n"+ myObj.name);
+//             System.out.println(myObj.age);
+//             System.out.println(myObj.year);
+//             myObj.study();
+//         }
+// }
