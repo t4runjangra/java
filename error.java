1967f7b7788b8372a41ab79021eb5620235aa844
tarun <tarunjangra1903@gmail.com>
2025-10-20 16:24:11 +0530

    errors in java

---
+++ error.java
@@ -0,0 +1,47 @@
+// three types of error in java
+// 1). compilation 2). runtime 3). logical 
+
+
+
+// public class error {
+//     public static void main(String[] args) {
+//         System.out.println(20/0);// error
+//     }
+// }
+
+
+// ********************************** Exceptions - try catch *************************************************
+
+
+
+// public class error {
+//   public static void main(String[ ] args) {
+//     try {
+//       int[] myNumbers = {1, 2, 3};
+//       System.out.println(myNumbers[10]);
+//     } catch (Exception e) {
+//       System.out.println("Something went wrong.");
+//     }
+//   }
+// }
+
+
+// throw 
+
+
+// public class error {
+//   static void checkAge(int age) {
+//     if (age < 18) {
+//       throw new ArithmeticException("Access denied - You must be at least 18 years old.");
+//     }
+//     else {
+//       System.out.println("Access granted - You are old enough!");
+//     }
+//   }
+
+//   public static void main(String[] args) {
+//     checkAge(15); 
+//   }
+// }
+
+
